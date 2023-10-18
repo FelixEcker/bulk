4ae6be3a3f8cfa956eaff95c9b6fc37b656d3a0d
FelixEcker <felix0eckert0.online.de@gmail.com>
2023-10-18 16:36:52 +0200

    setup buildfile

---
+++ src/bulk.c
@@ -0,0 +1,10 @@
+// bulk.c ; part of the bulk pager
+// Copyright (c) 2023, Marie Eckert
+// Licensed under the BSD 3-Clause license
+
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+  printf("test\n");
+  return 0;
+}
