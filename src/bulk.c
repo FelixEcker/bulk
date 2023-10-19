9da35b73b75cf77f8cc9210d985fe593aebe4edc
FelixEcker <felix0eckert0.online.de@gmail.com>
2023-10-19 17:14:31 +0200

    adjust source formatting

--- src/bulk.c
+++ src/bulk.c
@@ -2,19 +2,18 @@
 // Copyright (c) 2023, Marie Eckert
 // Licensed under the BSD 3-Clause license
 
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <termios.h>
-#include <sys/ioctl.h>
-#include <unistd.h>
 #include <signal.h>
-#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <termios.h>
+#include <unistd.h>
 
 #include <xmem.h>
 
 #define FALSE 0
-#define TRUE  1
+#define TRUE 1
 
 // The base size of the input buffer to be allocated
 #define BASE_BUFF_SIZE 4096
@@ -45,7 +44,7 @@ typedef struct bulk_t {
   char *buff;
   size_t buff_size;
   size_t buff_allocd;
-  
+
   // Pages
   size_t *pages;
   size_t page_count;
@@ -65,13 +64,15 @@ void signal_handler(int signum) {
 // INPUT FUNCTIONS
 
 static int advance_page(bulk_t *bulk) {
-  if (bulk->current_page + 1 == bulk->page_count) return FALSE;
+  if (bulk->current_page + 1 == bulk->page_count)
+    return FALSE;
   bulk->current_page++;
   return TRUE;
 }
 
 static int regress_page(bulk_t *bulk) {
-  if (bulk->current_page == 0) return FALSE;
+  if (bulk->current_page == 0)
+    return FALSE;
   bulk->current_page--;
   return TRUE;
 }
@@ -109,7 +110,8 @@ static void show(bulk_t *bulk) {
   unsigned char nrow = 0;
   size_t chr;
   for (chr = page_start; chr < bulk->buff_size; chr++) {
-    if (bulk->buff[chr] == '\n') nrow++;
+    if (bulk->buff[chr] == '\n')
+      nrow++;
     printf("%.*s", 1, bulk->buff + chr);
     if (nrow >= bulk->nrows) {
       page_ended = TRUE;
@@ -118,12 +120,14 @@ static void show(bulk_t *bulk) {
   }
 
   if (page_ended == TRUE && bulk->current_page + 1 >= bulk->page_count) {
-    bulk->pages = xrealloc(bulk->pages, sizeof(size_t) * (bulk->page_count + 1));
+    bulk->pages =
+        xrealloc(bulk->pages, sizeof(size_t) * (bulk->page_count + 1));
     bulk->pages[bulk->page_count] = chr + 1;
     bulk->page_count++;
   }
 
-  for (; nrow < bulk->nrows; nrow++) printf("\n");
+  for (; nrow < bulk->nrows; nrow++)
+    printf("\n");
   printf("PAGE %zu/%zu\n", bulk->current_page + 1, bulk->page_count);
 }
 
@@ -131,7 +135,7 @@ static void setup(bulk_t *bulk) {
   struct termios new_term_attr;
   tcgetattr(2, &bulk->orig_term_attr);
   memcpy(&new_term_attr, &bulk->orig_term_attr, sizeof(struct termios));
-  new_term_attr.c_lflag &= ~(ECHO|ICANON);
+  new_term_attr.c_lflag &= ~(ECHO | ICANON);
   new_term_attr.c_cc[VTIME] = 0;
   new_term_attr.c_cc[VMIN] = 0;
   tcsetattr(2, TCSANOW, &new_term_attr);
@@ -145,16 +149,16 @@ static void teardown(bulk_t bulk) {
 
 int main(int argc, char **argv) {
   bulk_t bulk = {
-    .quit = FALSE,
-    .color_enabled = TRUE,
-    .style_enabled = TRUE,
-    .minimal_mode  = FALSE,
-    .buff = xmalloc(BASE_BUFF_SIZE),
-    .buff_size = 0,
-    .buff_allocd = BASE_BUFF_SIZE,
-    .pages = xmalloc(sizeof(size_t)),
-    .page_count = 1,
-    .current_page = 0,
+      .quit = FALSE,
+      .color_enabled = TRUE,
+      .style_enabled = TRUE,
+      .minimal_mode = FALSE,
+      .buff = xmalloc(BASE_BUFF_SIZE),
+      .buff_size = 0,
+      .buff_allocd = BASE_BUFF_SIZE,
+      .pages = xmalloc(sizeof(size_t)),
+      .page_count = 1,
+      .current_page = 0,
   };
   setup(&bulk);
 
@@ -169,15 +173,15 @@ int main(int argc, char **argv) {
     bulk.nrows -= 1;
 
   while (bulk.quit == FALSE && gv_stop == FALSE) {
-    size_t bytes_read = read(STDIN_FILENO, bulk.buff+bulk.buff_size, 
-                             READ_CHUNK_SIZE);
+    size_t bytes_read =
+        read(STDIN_FILENO, bulk.buff + bulk.buff_size, READ_CHUNK_SIZE);
     bulk.buff_size += bytes_read;
     if (bulk.buff_size >= bulk.buff_allocd) {
       bulk.buff = xrealloc(bulk.buff, bulk.buff_allocd + READ_CHUNK_SIZE);
       bulk.buff_allocd += READ_CHUNK_SIZE;
     }
 
-    if (process_inputs(&bulk) != TRUE && bytes_read <= 0) 
+    if (process_inputs(&bulk) != TRUE && bytes_read <= 0)
       continue;
 
     show(&bulk);
--- src/xmem.c
+++ src/xmem.c
@@ -7,29 +7,32 @@
 
 void *xmalloc(size_t size) {
   void *ret = malloc(size);
-  if (ret == NULL) abort();
+  if (ret == NULL)
+    abort();
 
   return ret;
 }
 
 void *xcalloc(size_t nmemb, size_t size) {
   void *ret = calloc(nmemb, size);
-  if (ret == NULL) abort();
+  if (ret == NULL)
+    abort();
 
   return ret;
 }
- 
+
 void *xrealloc(void *ptr, size_t size) {
   void *ret = realloc(ptr, size);
-  if (ret == NULL) abort();
+  if (ret == NULL)
+    abort();
 
   return ret;
 }
 
 void *xreallocarray(void *ptr, size_t nmemb, size_t size) {
   void *ret = reallocarray(ptr, nmemb, size);
-  if (ret == NULL) abort();
+  if (ret == NULL)
+    abort();
 
   return ret;
-
 }
